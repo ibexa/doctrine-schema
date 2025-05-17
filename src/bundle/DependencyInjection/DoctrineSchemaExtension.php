d9b789c32dc6ed58c0d7056a46a961982a71d6b1
Adam Wójs <adam@wojs.pl>
2025-05-17 14:20:52 +0200

    [symfony/http-kernel] Replaced usage of internal Symfony\Component\HttpKernel\DependencyInjection\Extension class (#34)

--- src/bundle/DependencyInjection/DoctrineSchemaExtension.php
+++ src/bundle/DependencyInjection/DoctrineSchemaExtension.php
@@ -11,8 +11,8 @@ namespace Ibexa\Bundle\DoctrineSchema\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class DoctrineSchemaExtension extends Extension
 {
