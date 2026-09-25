fd4370528e500660e1a2147b4e5a4cb889341a15
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-09-25 18:24:02 +0200

    IBX-11939: Added sqlite platform support to ibexa:doctrine:schema:dump-sql
    Rebuilt as a single commit on the refreshed base; replaces these 1 commits:
    
    - IBX-11939: Added SQLite to ibexa:doctrine:schema:dump-sql platforms

--- src/bundle/Command/DumpSqlCommand.php
+++ src/bundle/Command/DumpSqlCommand.php
@@ -14,6 +14,7 @@ use Doctrine\DBAL\Platforms\MariaDBPlatform;
 use Doctrine\DBAL\Platforms\MySQL80Platform;
 use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SQLitePlatform;
 use Ibexa\DoctrineSchema\Builder\SchemaBuilder;
 use InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -39,6 +40,7 @@ final class DumpSqlCommand extends Command
         'mysql' => MySQLPlatform::class,
         'mariadb' => MariaDBPlatform::class,
         'postgres' => PostgreSQLPlatform::class,
+        'sqlite' => SQLitePlatform::class,
     ];
 
     public function __construct(Connection $db, SchemaBuilder $schemaBuilder)
